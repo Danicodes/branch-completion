007c65548ba385385799121eea1f8a1c5d8161df
danicodes94 <danicodes94@gmail.com>
2023-07-29 10:33:48 -0400

    Initial push

---
+++ bash_complete.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+BRANCH_COMPLETE=($(git branch --sort=-committerdate |\
+                    head -n 20 ))
+branch_complete(){
+    searchW=${COMP_WORDS[1]}
+    COMPREPLY=COMPGEN -W "${BRANCH_COMPLETE}" "${searchW}"
+
+}
+for ((i=0; i<10;i++)); do
+    echo ${BRANCH_COMPLETE[i]}
+done
+#complete -o default -W "${BRANCH_COMPLETE[*]}" git checkout 
