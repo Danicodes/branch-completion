ac1b32782eaa58cf53934365318bf58698a15f11
danicodes94 <danicodes94@gmail.com>
2023-07-29 10:46:20 -0400

    Test branch for completion

--- bash_complete.sh
+++ bash_complete.sh
@@ -3,10 +3,10 @@ BRANCH_COMPLETE=($(git branch --sort=-committerdate |\
                     head -n 20 ))
 branch_complete(){
     searchW=${COMP_WORDS[1]}
-    COMPREPLY=COMPGEN -W "${BRANCH_COMPLETE}" "${searchW}"
+    COMPREPLY=COMPGEN -W "${BRANCH_COMPLETE[@]}" "${searchW}"
 
 }
-for ((i=0; i<10;i++)); do
+for ((i=0; i<${#BRANCH_COMPLETE[@]};i++)); do
     echo ${BRANCH_COMPLETE[i]}
 done
 #complete -o default -W "${BRANCH_COMPLETE[*]}" git checkout 
