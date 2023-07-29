fd57bbc37e8e34b0a0ffff357265f4974aba008b
danicodes94 <danicodes94@gmail.com>
2023-07-29 16:35:36 -0400

    Removed comments

--- bash_complete.sh
+++ bash_complete.sh
@@ -1,11 +1,7 @@
 #!/usr/bin/env bash
 branch_complete(){
     if [[ "${COMP_WORDS[1]}" == "checkout" && "${#COMP_WORDS[@]}" == "2" ]]; then
-    #BRANCH_COMPLETE=($(git branch --sort=-committerdate |\
-    #                head -n 20 ))
     COMPREPLY=($(compgen -o nospace -W "$(git branch --sort=-committerdate)" ))
-
-    #COMPREPLY=($(compgen -W "${BRANCH_COMPLETE[@]}" -- "{COMP_WORDS[1]}"))
     fi
 }
 complete -r default -o nospace -F branch_complete git
